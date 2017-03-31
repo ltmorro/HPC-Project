fa5077a1f8a34e83fa61a8a4adbe261712a0e016
Foster McLane <fmclane@clemson.edu>
2017-03-31 13:08:18 -0400

    add basic comet.py

---
+++ comet.py
@@ -0,0 +1,33 @@
+import geni.portal as portal
+import geni.rspec.pg as rspec
+import geni.rspec.igext as IG
+
+pc = portal.Context()
+request = rspec.Request()
+
+tourDescription = 'This profile sets up a mini cluster modeled after SDSC Comet.'
+
+tourInstructions = \
+"""
+### Basic Instructions
+None yet.
+"""
+
+tour = IG.Tour()
+tour.Description(IG.Tour.TEXT,tourDescription)
+tour.Instructions(IG.Tour.MARKDOWN,tourInstructions)
+request.addTour(tour)
+
+link = request.LAN('lan')
+
+for i in range(10):
+    node = request.RawPC('node' + str(i))
+    node.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS72-64-STD'
+    iface = node.addInterface('if' + str(i))
+    iface.component_id = 'eth1'
+    iface.addAddress(rspec.IPv4Address('192.168.1.' + str(i + 1), '255.255.255.0'))
+    link.addInterface(iface)
+
+    node.addService(rspec.Execute(shell='/bin/sh', command='sudo echo hi'))
+
+portal.context.printRequestRSpec(request)
