afbccd500d277d55254aa0879da580193b6b49de
Foster McLane <fmclane@clemson.edu>
2017-04-04 21:02:52 -0400

    add allocated resources

--- profile.py
+++ profile.py
@@ -21,17 +21,30 @@ request.addTour(tour)
 link = request.LAN('lan')
 
 for i in range(10):
-    node = request.RawPC('node' + str(i))
+    node = request.XenVM('node' + str(i))
     node.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS72-64-STD'
     iface = node.addInterface('if' + str(i))
-    iface.component_id = 'eth1'
     iface.addAddress(rspec.IPv4Address('192.168.1.' + str(i + 1), '255.255.255.0'))
     link.addInterface(iface)
 
     if i == 0:
         node.addService(rspec.Execute(shell='/bin/sh', command='sudo echo login >/root/designation'))
+
+        node.cores = 1
+        node.ram = 1024
+        node.disk = 4
+
+        node.routable_control_ip = True
     elif i == 1:
         node.addService(rspec.Execute(shell='/bin/sh', command='sudo echo storage >/root/designation'))
+
+        node.cores = 1
+        node.ram = 8192
+        node.disk = 4
+
+        bs = node.Blockstore('bs', '/mydata')
+        bs.size = '1024GB'
+
         node.addService(rspec.Execute(shell='/bin/sh', command='yum install nfs-utils nfs-utils-lib'))
         node.addService(rspec.Execute(shell='/bin/sh', command='chkconfig nfs on; service rpcbind start; service nfs start'))
         node.addService(rspec.Execute(shell='/bin/sh', command='mkdir /storage'))
@@ -39,9 +52,21 @@ for i in range(10):
         node.addService(rspec.Execute(shell='/bin/sh', command='exportfs -a'))
     elif i == 2:
         node.addService(rspec.Execute(shell='/bin/sh', command='sudo echo gpu >/root/designation'))
+
+        node.cores = 2
+        node.ram = 8192
+        node.disk = 4
     elif i == 3:
         node.addService(rspec.Execute(shell='/bin/sh', command='sudo echo large memory >/root/designation'))
+
+        node.cores = 8
+        node.ram = 65536
+        node.disk = 4
     else:
         node.addService(rspec.Execute(shell='/bin/sh', command='sudo echo compute >/root/designation'))
+
+        node.cores = 2
+        node.ram = 8192
+        node.disk = 4
 
 portal.context.printRequestRSpec(request)
