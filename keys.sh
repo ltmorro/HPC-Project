e8f13db86e44535f9ac6b77d1d6b2b02d47bb80b
Foster McLane <fmclane@clemson.edu>
2017-04-07 18:42:49 -0400

    fix keys.sh

--- keys.sh
+++ keys.sh
@@ -2,5 +2,5 @@
 cd /users
 
 find * -mindepth 0 -maxdepth 0 -exec sudo -u {} mkdir -p /users/{}/.ssh \;
-find * -mindepth 0 -maxdepth 0 -exec sudo -u {} ssh-keygen -P \'\' \;
+yes '' | find * -mindepth 0 -maxdepth 0 -exec sudo -u {} ssh-keygen -P '' \;
 find * -mindepth 0 -maxdepth 0 -exec sudo -u {} sh -c 'cat /users/{}/.ssh/id_rsa.pub | tee -a /users/{}/.ssh/authorized_keys' \;
